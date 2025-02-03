3d421bc9a482171f0492671ab6f2c552fcce404c
LDxmrts <87765239+LDxmrts@users.noreply.github.com>
2025-02-03 22:38:47 +0000

    tabuada

---
+++ Atividades/Mini-projetos/Repeticao-For_Tabuada.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int main (){
+
+    int numero, i;
+
+    printf ("Digite um número para calcularmos a tabuada...\n");
+    scanf ("%d", &numero);
+
+
+    //Tabuada
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d x %d = %d\n", i, numero, i*numero);                    
+    }
+
+
+
+
+
+
+}
