0b698f72dbbad9347ece6239262218b51de2f9de
LDxmrts <87765239+LDxmrts@users.noreply.github.com>
2025-02-03 20:21:03 +0000

    commit for repeticao

---
+++ Atividades/Repeticao-FOR.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int main () {
+
+    
+    for (int i = 1; i <=10; i++){
+
+
+
+        printf ("%d\n", i);
+
+
+
+    }
+
+    for (int j = 1; j <= 20; j++){
+        if (j % 2 == 0){
+
+            printf ("Números pares entre 1 e 20: %d\n", j);
+
+
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}
